4f2455a5334454549191898bfdbe037f5e8cc3f0
Niyas Mohammed <niaz.angels@gmail.com>
2019-11-07 23:51:09 +0530

    Completes Layers

--- bloo/layers.py
+++ bloo/layers.py
@@ -6,12 +6,15 @@ propagate gradients backwards.
 For example a neural net may look like:
 inputs -> Linear -> Tanh -> Linear -> Softmax
 """
-import numpy as numpy
+import numpy as np
+from typing import Dict
 from bloo.tensor import Tensor
 
 
 class Layer:
     def __init__(self) -> None:
+        self.params: Dict[str, Tensor] = {}
+        self.grads: Dict[str, Tensor] = {}
         pass
 
     def forward(self, input: Tensor) -> Tensor:
@@ -25,3 +28,36 @@ class Layer:
         Backpropagate this gradient through the layers
         """
         raise NotImplementedError
+
+
+class Linear(Layer):
+    """
+    Computes output = inputs @ w + b
+    """
+
+    def __init__(self, input_size: int, output_size: int) -> None:
+        # inputs will be (batch_size, input_size)
+        # outputs will be (batch_size, output_size)
+        super().__init__()
+        self.params["w"] = np.random.randn(input_size, output_size)
+        self.params["b"] = np.random.randn(output_size)
+
+    def forward(self, input: Tensor) -> Tensor:
+        self.input = input
+        return input @ self.params["w"] + self.params["b"]
+
+    def backward(self, grad: Tensor) -> Tensor:
+        """
+        if y = f(x) and x = a * b + c
+        then dy/db = b * f'(x)
+        and  dy/da = a * f'(x)
+        and  dy/dc = f'(x)
+        
+        if y = f(x) and x = a @ b + c
+        then dy/da = f'(x) @ b.T
+        and dy/dc = f'(x)
+        """
+        # grads are (batch_size x output_size)
+        self.grads["b"] = np.sum(grad, axis=0)
+        self.grads["w"] = self.input.T @ grad
+        return grad @ self.params["w"].T
